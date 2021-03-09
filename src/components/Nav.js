a2173d56523cabfae28275f0e2f9db1f153264e8
Elin Amren <elin.amren@gmail.com>
2021-03-09 12:56:44 +0100

    navbarstyle and structure

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,5 +1,35 @@
+import feed from "../images/feed-icon.png";
+import profile from "../images/profile-icon.png";
+import search from "../images/search-icon.png";
+import menu from "../images/menu-icon.png";
+
 const Nav = () => {
-  return <h1>Hej</h1>;
+  return (
+    <nav>
+      <ul className="navbar">
+        <li>
+          <a href="/">
+            <img src={feed} alt="feed icon" />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <img src={profile} alt="profile icon" />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <img src={search} alt="search icon" />
+          </a>
+        </li>
+        <li>
+          <a href="/">
+            <img src={menu} alt="menu icon" />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default Nav;
